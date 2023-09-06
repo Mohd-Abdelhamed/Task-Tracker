a77081ecea3a83f45461684427c6c381256cf7b8
Mohamed Abdelmawla <42118436+Mohd-Abdelhamed@users.noreply.github.com>
2023-09-06 17:08:11 +0300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <h3>Version 1.0.0</h3>
+  <h3>Developed by <a href="https://www.facebook.com/3b7ameed.1998/" target="_blank">3b7ameed</a></h3>
   <router-link to="/">Go Back</router-link>
 </template>
 
